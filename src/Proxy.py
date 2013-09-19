948859e8b0fed1d00c8d85a7a89c6e487faf34a2
Cesar Ramirez <ramirez@cesarandres.com>
2013-09-19 13:13:58 -0500

    Inital part, Python3 compat

--- src/Proxy.py
+++ src/Proxy.py
@@ -4,24 +4,48 @@ PROXY_NAME = 'Perry, the Python Proxy'
 HTTP_VERSION = 'HTTP/1.1'
 BUFFER_LENGHT = 1024 * 8
 
+import threading
+import socket
+
 class ProxyConnection:
-   def __init__(self, connection, address, timeout):
-      pass
+	def __init__(self, connection, address, timeout):
+		self.local_connection = connection
+		self.remote_connection  = None
+		self.conn_buffer = ''
+		self.timeout = timeout
 
-   def get_base_header(self):
-      pass
+		data = self._get_base_header()
+		self.method = data[0]
+		self.path = data[1]
+		self.protocol = data [2]
 
-   def process(self):
-      pass
+		self._forwarding()
 
-   def remoteConnect(self, host):
-      pass
+		self._remote_connection.close()
+		self._local_connection.close()
 
-   def localConnect(self):
-      pass
+	def _get_base_header(self):
+		pass
+
+	def _forwarding(self):
+		pass
+
+	def _remoteConnect(self, host):
+		pass
+
+	def _localConnect(self):
+		pass
 
 def start(host='localhost', port=1234, IPv6=False, timeout=30, handler=ProxyConnection):
-   pass
+	if IPv6:
+		socket_family = socket.AF_INET6
+	else:
+		socket_family = socket.AF_INET
+	
+	listen_socket = socket(family=socket_family, sockaddr=(host, port))
+	
+	while True:
+		Thread(target=handler,args=(listen_socket.connect()+timeout)).start()
 
 if __name__ == '__main__':
-   start()
+	start()
