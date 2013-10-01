db274b8288028fb106a2cd32b0e5ae9c779ab2bb
Cesar Ramirez <ramirez@cesarandres.com>
2013-09-30 20:52:01 -0500

    WIP

--- src/Proxy.py
+++ src/Proxy.py
@@ -4,8 +4,77 @@ PROXY_NAME = 'Perry, the Python Proxy'
 HTTP_VERSION = 'HTTP/1.1'
 BUFFER_LENGTH = 1024 * 8
 
-import threading, socket, sys, re, signal, datetime, hashlib
+import threading
+import socket
+import sys
+import re
+import signal
+import datetime
+import hashlib
+import pdb
 
+class HttpRequest:
+    def __init__(self, decoded_request):
+        self.method = (decoded_request).splitlines()[0].split()[0]
+        self.request_uri = (decoded_request).splitlines()[0].split()[1]
+        self.http_version = (decoded_request).splitlines()[0].split()[2]
+        self.request_headers = dict()
+        
+        tmp = (decoded_request).splitlines()[1:-1]
+        for line in tmp:
+            self.request_headers[line.split(':')[0]] = line.split(':')[1].strip()
+        
+        self.message_body = (decoded_request).splitlines()[-1]
+        self.request_line = [self.method, self.request_uri, self.http_version]
+
+    def strip_cache_headers(self):
+        if 'If-Modified-Since' in self.request_headers:
+            del self.request_headers['If-Modified-Since']
+        if 'If-None-Match' in self.request_headers:
+            del self.request_headers['If-None-Match']
+        if 'Cache-Control' in self.request_headers:
+            del self.request_headers['Cache-Control']
+        
+    def strip_user_agent(self):
+        del self.request_headers['User-Agent']
+
+    def get_host_name(self):
+        host_and_file = self.request_uri[self.request_uri.find('//')+2:]
+        backslash_index = host_and_file.find('/')
+        return host_and_file[:backslash_index]
+        
+    def get_modified_request(self):
+        first_newline = decoded_request.find('\n')
+        first_line = decoded_request[:first_newline]
+        split_first_line = first_line.split()
+        self.method = split_first_line[0]
+        self.path = split_first_line[1]
+        self.protocol = split_first_line[2]
+
+        host_and_file = self.path[self.path.find('//')+2:]
+
+        backslash_index = host_and_file.find('/')
+        self.host = host_and_file[:backslash_index]
+        self.requested_file = host_and_file[backslash_index:]
+            
+        modified_first_line = "%s %s %s" % (self.method, self.requested_file, self.protocol)
+        decoded_request = modified_first_line + decoded_request[first_newline:]
+
+    def get_original_request_line(self):
+        return "%s %s %s" % (self.method, self.request_uri, self.http_version)
+    
+    def get_modified_request_line(self):
+        return "%s %s %s" % (self.method, self.request_uri, self.http_version)
+    
+    def get_request_headers(self):
+        headers_text = ''
+        for line in tmp:
+            self.request_headers[line.split(':')[0]] = line.split(':')[1].strip()
+        return headers_text
+    
+    def get_message_body(self):
+        return self.body
+        
 class ClientRequest:
 
     def __init__(self, local_conn, address, strip_cache_headers, strip_user_agent):
@@ -15,78 +84,28 @@ class ClientRequest:
         self.socket_type = local_conn.type
         self.address = address
         self.port = 80
-        self.strip_cache_headers = strip_cache_headers
-        self.strip_user_agent = strip_user_agent
-
-        self.client_request = local_conn.recv(BUFFER_LENGTH)
-
-        decoded_client_request = bytes.decode(self.client_request)
-
-        # modify request file URL (and anonymize, if applicable)
-        self.modify_request(decoded_client_request)
-
-    def modify_request(self, decoded_request):
-
-        first_newline = decoded_request.find('\n')
-        first_line = decoded_request[:first_newline]
-                
-        if first_line.find('GET') != -1:
-
-            print("--- Client -> Proxy Request ---\n%s" % (decoded_request))
-                        
-            split_first_line = first_line.split()
-            self.method = split_first_line[0]
-            self.path = split_first_line[1]
-            self.protocol = split_first_line[2]
-
-            host_and_file = self.path[self.path.find('//')+2:]
-
-            backslash_index = host_and_file.find('/')
-            self.host = host_and_file[:backslash_index]
-            self.requested_file = host_and_file[backslash_index:]
-                
-            modified_first_line = "%s %s %s" % (self.method, self.requested_file, self.protocol)
-            decoded_request = modified_first_line + decoded_request[first_newline:]
-
-            # changed "Connection: keep-alive" to "Connection: close" (with 'keep-alive', the recv() from the remote server would take forever,
-            # since it would block until the remote server closed the connection). Another way to fix the problem would be to actually 
-            # handle a 'keep-alive' connection properly, but this works for now
-            decoded_request = re.sub(r'Connection: keep-alive', 'Connection: close', decoded_request, flags=(re.IGNORECASE | re.MULTILINE) )
-
-            if self.strip_cache_headers:
-                # strip 'If-Modified-Since', 'If-None-Match', and 'Cache-Control' fields
-                decoded_request = re.sub(r'If-Modified-Since:.*?\n', '', decoded_request, flags=(re.IGNORECASE | re.MULTILINE))
-                decoded_request = re.sub(r'If-None-Match:.*?\n', '', decoded_request, flags=(re.IGNORECASE | re.MULTILINE))
-                decoded_request = re.sub(r'Cache-Control:.*?\n', '', decoded_request, flags=(re.IGNORECASE | re.MULTILINE))
-
-            if self.strip_user_agent:
-                # strip 'User-Agent' field
-                decoded_request = re.sub(r'User-Agent:.*?\n', '', decoded_request, flags=(re.IGNORECASE | re.MULTILINE))
-
-            self.client_request = str.encode(decoded_request)                        
-
-        else:
-            self.method = None
-
-    def execute(self, lock, cache):
-
-        try:
+         = local_conn.recv(BUFFER_LENGTH)        
+        self.decoded_client_request = HttpRequest(bytes.decode(self.client_request))
+        if strip_cache_headers:
+            self.decoded_client_request.strip_cache_headers()
+        if strip_user_agent:
+            self.decoded_client_request.strip_user_agent()
             
-            if self.method == 'GET':
+    def execute(self, lock, cache):
+        try:
+
+            host = self.decoded_client_request.get_host_name()
+            if self.decoded_client_request.method == 'GET':
 
                 md5hash = hashlib.md5()
                 md5hash.update(self.client_request)
                 request_digest = md5hash.digest()
-                response_size = 0
-            
-                # convert hostname to IP address
-                ip = socket.gethostbyname(self.host)
-                
+                response_size = 01
+                            
                 if request_digest not in cache: # connect to remote server
                     remote_conn = socket.socket(self.socket_family, self.socket_type)
-                    remote_conn.connect((self.host, self.port))
+                    remote_conn.connect((host, self.port))
                     print("--- Proxy -> Server Request ---\n%s" % (bytes.decode(self.client_request)))
-                        
                     request_length = len(self.client_request)
                                 
                     total_sent = 0
@@ -107,7 +126,8 @@ class ClientRequest:
                             break
 
                     response_size = len(response)
-                    print("--- Server Response ---\n%s\n" % repr(response))
+                    print("--- Server Response ---\n%s\n" % bytes.decode(response))
+                    pdb.set_trace()
                     
                     remote_conn.close()
                     cache[request_digest] = response
@@ -121,12 +141,17 @@ class ClientRequest:
                         sent = self.local_conn.send(cached)
                         total_sent += sent
 
-                    print("** %s%s served from cache.\n" % (self.host, self.requested_file))
+                    print("** %s%s served from cache.\n" % (host, self.requested_file))
 
+                # convert hostname to IP address
+                #
+                
+                #ip = socket.gethostbyname(host)
+                
                 # acquire lock and write to file
-                lock.acquire()
-                log_file.write("%s %s %i\n" % (str(datetime.datetime.now()), ip, response_size))
-                lock.release()
+                #lock.acquire()
+                #log_file.write("%s %s %i\n" % (str(datetime.datetime.now()), ip, response_size))
+                #lock.release()
                 
         except OSError: 
             # exit gracefully
