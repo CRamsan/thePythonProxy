1d00929bc2fc16e7a370e24332b64b79c1991ebe
Josh Tan <jtan189@gmail.com>
2013-10-06 21:48:20 -0500

    Updated README and reference script.

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 thePythonProxy
 ==============
+
+Prerequisites
+-------------
 This proxy is written using Python3. Make sure that you have it installed.
+
+References
+----------
+The official Python documentation on socket programming was used extensively:
+http://docs.python.org/3.3/howto/sockets.html
+
+In addition, open-source program *python-proxy*, developed by Fábio Domingues,
+was used to guide certain parts of our implementation:
+https://code.google.com/p/python-proxy/
+
+This code for this program has been placed in the references/ directory,
+for your convenience.
+
+
+Running the Proxy
+-----------------
 
 To run it simply call it like this: 
  - ./Proxy.py
--- reference/PythonProxy.py
+++ reference/PythonProxy.py
@@ -1,3 +1,86 @@
+# -*- coding: cp1252 -*-
+# <PythonProxy.py>
+#
+#Copyright (c) <2009> <Fábio Domingues - fnds3000 in gmail.com>
+#
+#Permission is hereby granted, free of charge, to any person
+#obtaining a copy of this software and associated documentation
+#files (the "Software"), to deal in the Software without
+#restriction, including without limitation the rights to use,
+#copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the
+#Software is furnished to do so, subject to the following
+#conditions:
+#
+#The above copyright notice and this permission notice shall be
+#included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#OTHER DEALINGS IN THE SOFTWARE.
+
+"""\
+Copyright (c) <2009> <Fábio Domingues - fnds3000 in gmail.com> <MIT Licence>
+
+                  **************************************
+                 *** Python Proxy - A Fast HTTP proxy ***
+                  **************************************
+
+Neste momento este proxy é um Elie Proxy.
+
+Suporta os métodos HTTP:
+ - OPTIONS;
+ - GET;
+ - HEAD;
+ - POST;
+ - PUT;
+ - DELETE;
+ - TRACE;
+ - CONENCT.
+
+Suporta:
+ - Conexões dos cliente em IPv4 ou IPv6;
+ - Conexões ao alvo em IPv4 e IPv6;
+ - Conexões todo o tipo de transmissão de dados TCP (CONNECT tunneling),
+     p.e. ligações SSL, como é o caso do HTTPS.
+
+A fazer:
+ - Verificar se o input vindo do cliente está correcto;
+   - Enviar os devidos HTTP erros se não, ou simplesmente quebrar a ligação;
+ - Criar um gestor de erros;
+ - Criar ficheiro log de erros;
+ - Colocar excepções nos sítios onde é previsível a ocorrência de erros,
+     p.e.sockets e ficheiros;
+ - Rever tudo e melhorar a estrutura do programar e colocar nomes adequados nas
+     variáveis e métodos;
+ - Comentar o programa decentemente;
+ - Doc Strings.
+
+Funcionalidades futuras:
+ - Adiconar a funcionalidade de proxy anónimo e transparente;
+ - Suportar FTP?.
+
+
+(!) Atenção o que se segue só tem efeito em conexões não CONNECT, para estas o
+ proxy é sempre Elite.
+
+Qual a diferença entre um proxy Elite, Anónimo e Transparente?
+ - Um proxy elite é totalmente anónimo, o servidor que o recebe não consegue ter
+     conhecimento da existência do proxy e não recebe o endereço IP do cliente;
+ - Quando é usado um proxy anónimo o servidor sabe que o cliente está a usar um
+     proxy mas não sabe o endereço IP do cliente;
+     É enviado o cabeçalho HTTP "Proxy-agent".
+ - Um proxy transparente fornece ao servidor o IP do cliente e um informação que
+     se está a usar um proxy.
+     São enviados os cabeçalhos HTTP "Proxy-agent" e "HTTP_X_FORWARDED_FOR".
+
+"""
+
 import socket, thread, select
 
 __version__ = '0.1.0 Draft 1'
@@ -25,10 +108,8 @@ class ConnectionHandler:
             end = self.client_buffer.find('\n')
             if end!=-1:
                 break
-        print self.client_buffer
-        #print '%s'%self.client_buffer[:end]#debug
+        print '%s'%self.client_buffer[:end]#debug
         data = (self.client_buffer[:end+1]).split()
-        print data
         self.client_buffer = self.client_buffer[end+1:]
         return data
 
@@ -83,7 +164,7 @@ class ConnectionHandler:
             if count == time_out_max:
                 break
 
-def start_server(host='localhost', port=8081, IPv6=False, timeout=60,
+def start_server(host='localhost', port=8080, IPv6=False, timeout=60,
                   handler=ConnectionHandler):
     if IPv6==True:
         soc_type=socket.AF_INET6
