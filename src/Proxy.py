c3ca43f0fbfca2d3941c4c2f89a7575020555c97
Josh Tan <jtan189@gmail.com>
2013-10-07 20:42:17 -0500

    Source code commented.

--- src/Proxy.py
+++ src/Proxy.py
@@ -19,31 +19,16 @@ import pdb
 from time import sleep
 
 class Cache:
-
     '''
     This cache follows a Least-Recently-Used model with all operations running 
-    in constant time under optimal circumstances.
-
-    Each entry of the cache contains the following attributes:
-    
-    ``key``
-         the name of the file that contains the cached data
-
-    ``size``
-         the size in bytes of the content in the file
-
-    ``prev_entry``
-         pointer to previous entry
-
-    ``next_entry``
-         pointer to next entry
+    in constant time under optimal circumstances. It consists of queue, implemented
+    as a doubly linked list, as well as a dictionary mapping response hash IDs to
+    the entries.
 
     This structure allows to keep track of all the entries in the way of a queue. 
     Each entry is apended at the begining of the queue and when the cache is full
-    the last entry is removed.
-
-    There is also a dictionary that will keep track of each entry, with this we can
-    achieve reading operations in constant time.
+    the last entry is removed. Furthermore, the entry dictionary is used to achieve
+    reading operations in constant time.
     '''
     
     def __init__(self, max_size):
@@ -55,12 +40,15 @@ class Cache:
         self.lock = threading.Lock()
 
     def touch(self, hashid):
+        '''
+        Move an entry to the front of the queue.
+        '''
+
         if hashid in self.table:
 
-            #Get references for the current entry, as well as the next 
-            #and previous ones
+            # Get references for the current entry, as well as the next 
+            # and previous ones
             touch_entry = self.table[hashid]
-
             prev_entry = touch_entry.prev_entry
             next_entry = touch_entry.next_entry
             
@@ -68,28 +56,30 @@ class Cache:
                 print ("Object is already first in cache")
                 return
 
-            #remove the reference to the current entry by making 
-            #the previous entry point straight to the next entry                                              
+            # remove the reference to the current entry by making 
+            # the previous entry point straight to the next entry                                              
             if prev_entry is not None:
                 prev_entry.next_entry = next_entry
 
-            #If the entry is not the last one
+            # If the entry is not the last one
             if next_entry is not None:
-                #remove the reference to the current entry by making 
-                #the next entry point straight to the previous entry 
+                # remove the reference to the current entry by making 
+                # the next entry point straight to the previous entry 
                 next_entry.prev_entry = prev_entry
 
-            # insert in front
+            # moved touched entry to the front
             if self.first is not None:
                 next_entry = self.first
                 self.first.prev_entry = touch_entry
-                
             prev_entry = None
             self.first = touch_entry
 
             print("%s moved to front of cache.\n" % (hashid))
 
     def insert(self, hashid, content, size):
+        '''
+        Insert a response entry into the queue.
+        '''
         
         if hashid not in self.table:
             if(size > self.max_size):
@@ -102,23 +92,27 @@ class Cache:
                     while True:
                         # Get the size and key of the entry to be removed
                         old_last = self.last
-                        old_last.acquire_lock()
                         del_size = old_last.size
                         del_key = old_last.key
+                        
                         # Remove the file
                         old_last.delete_file()                    
-                        #Move the self.last pointer to the previous entry
+                        
+                        # Move the self.last pointer to the previous entry
                         prev_entry = old_last.prev_entry
                         self.last = prev_entry 
-                        #Remove a reference to the previous-last entry from
-                        #the dictionary
-                        old_last.release_lock()
+                        
+                        # Remove a reference to the previous-last entry from
+                        # the dictionary
                         del self.table[del_key]
                         print("%s removed from cache.\n" % (del_key))
-                        #The last entry does not have a 'next' entry
+                        
+                        # The last entry does not have a 'next' entry
                         self.last.next_entry = None
-                        #Substract the file size
+                        
+                        # Substract the file size
                         self.current_size -= del_size
+                        
                         # Check if new entry will fit after last item was removed
                         if(self.current_size + size <= self.max_size):
                             break
@@ -130,7 +124,7 @@ class Cache:
                 new_first = self.Entry(hashid, size, None, None)
                 self.last = new_first
             else:
-                #Set the new entry as the first in the queue
+                # Set the new entry as the first in the queue
                 new_first = self.Entry(hashid, size, None, self.first)
                 self.first.prev_entry = new_first
             
@@ -140,23 +134,46 @@ class Cache:
             self.current_size += size
             print("%s added to cache.\n" % (hashid))
 
-        #If entry is already in queue    
+        # If entry is already in queue    
         else:
             self.touch(hashid)
     
     def get(self, hashid):
+        '''
+        Get the response associated with the hashid,
+        by reading it from cache file on disk.
+        '''
         content = None
         if hashid in self.table:
             found = self.table[hashid]
             self.touch(hashid)
             content = found.read_file()
-
         return content
     
     def queue(self):
+        '''
+        Print the current entries in the queue. Useful for debugging.
+        '''
         self.first.print_queue()
 
     class Entry:
+        '''
+
+        A cache entry. Each entry of the cache contains the following attributes:
+    
+        ``key``
+            the name of the file that contains the cached data
+
+        ``size``
+            the size in bytes of the content in the file
+
+        ``prev_entry``
+            pointer to previous entry
+
+        ``next_entry``
+            pointer to next entry
+
+        '''
 
         def __init__(self, key, size, prev_entry, next_entry):
             self.prev_entry = prev_entry
@@ -165,25 +182,47 @@ class Cache:
             self.next_entry = next_entry
 
         def read_file(self):
+            '''
+            Return the response contained in the file
+            associated with this entry.
+            '''
+
             cache_file = open("cache/"+str(self.key), 'rb')
             content = cache_file.read()
             cache_file.close()
             return content
             
         def create_file(self, data):
+            '''
+            Create a file on disk to store the response
+            associated with this entry.
+            '''
+
             cache_file = open("cache/"+str(self.key), 'wb')
             cache_file.write(data)
             cache_file.close()
 
         def delete_file(self):
+            '''
+            Delete this entry's cache file from the disk.
+            '''
+
             os.remove("cache/"+str(self.key))            
                         
         def print_queue(self):
+            '''
+            Print this entry's hashid, as well as the next.
+            Useful for debugging.
+            '''
             print (self.key)
             if self.next_entry is not None:
                 self.next_entry.print_queue()
         
 class HttpRequest:
+    '''
+    An HTTP request. Contains the request headers and body.
+    Headers are contained in a dict object.
+    '''
 
     def __init__(self, decoded_request):
         firstline_split = (decoded_request).splitlines()[0].split()
@@ -202,6 +241,11 @@ class HttpRequest:
         self.request_line = [self.method, self.request_uri, self.http_version]
 
     def strip_cache_headers(self):
+        '''
+        Strip the headers related to caching, since this will
+        be performed by the proxy.
+        '''
+
         if 'If-Modified-Since' in self.request_headers:
             del self.request_headers['If-Modified-Since']
         if 'If-None-Match' in self.request_headers:
@@ -210,20 +254,37 @@ class HttpRequest:
             del self.request_headers['Cache-Control']
 
     def set_connection_close(self):
+        '''
+        Set the HTTP request to use a non-persistent connection.
+        '''
+
         if 'Connection' in self.request_headers:
                 self.request_headers['Connection'] = 'close'
         if 'Proxy-Connection' in self.request_headers:
                 self.request_headers['Proxy-Connection'] = 'close'
         
     def strip_user_agent(self):
+        '''
+        Strip the user agent header, for increased anonymity.
+        '''
+
         del self.request_headers['User-Agent']
 
     def get_host_name(self):
+        '''
+        Get the host name associated with this HTTP request.
+        '''
+
         host_and_file = self.request_uri[self.request_uri.find('//')+2:]
         backslash_index = host_and_file.find('/')
         return host_and_file[:backslash_index]
 
     def get_port(self):
+        '''
+        Get the port this HTTP request should use. Default to port 80,
+        if not specified.
+        '''
+
         host = self.get_host_name()
         i = host.find(':')
         if i != -1:
@@ -232,36 +293,67 @@ class HttpRequest:
             return 80
         
     def get_modified_request(self):
+        '''
+        Return the HTTP request, modified for transmission
+        from the proxy to the server.
+        '''
+
         modified_request = self.get_modified_request_line()
         modified_request += self.get_request_headers()
         modified_request += self.get_message_body()
         return modified_request
         
     def get_original_request(self):
+        '''
+        Return the HTTP request, as originally received from
+        the client.
+        '''
+        
         original_request = self.get_original_request_line()
         original_request += self.get_request_headers()
         original_request += self.get_message_body()
         return original_request
 
     def get_original_request_line(self):
+        '''
+        Get the initial line of the original request.
+        '''
+
         return "%s %s %s \r\n" % (self.method, self.request_uri, self.http_version)
     
     def get_modified_request_line(self):
+        '''
+        Get the inital line of the modified request. In this modified version, the host 
+        is stripped from the request URI.
+        '''
+
         host_and_file = self.request_uri[self.request_uri.find('//')+2:]
         backslash_index = host_and_file.find('/')
         self.requested_file = host_and_file[backslash_index:]
         return "%s %s %s \r\n" % (self.method, self.requested_file, self.http_version)
     
     def get_request_headers(self):
+        '''
+        Return all headers as a single string.
+        '''
+
         headers_text = ""
         for param in self.request_headers.keys():
             headers_text+= ("%s: %s \r\n" % (param, self.request_headers[param]))
         return headers_text
     
     def get_message_body(self):
+        '''
+        Return the message body of the request.
+        '''
+        
         return (self.message_body+"\r\n")
         
 class ClientRequest:
+    '''
+    A client request. Contains information on the client, as well as the actual
+    HTTP request made.
+    '''
 
     def __init__(self, local_conn, address, strip_cache_headers, strip_user_agent):
 
@@ -279,6 +371,7 @@ class ClientRequest:
         self.decoded_client_request = HttpRequest(bytes.decode(local_request))
         self.port = self.decoded_client_request.get_port()
 
+        # strip cache and user-agent headers, if applicable
         if strip_cache_headers:
             self.decoded_client_request.strip_cache_headers()
         if strip_user_agent:
@@ -290,7 +383,14 @@ class ClientRequest:
         print("--- Client -> Proxy Request ---\n%s" % (self.decoded_client_request.get_original_request()))
                     
     def execute(self, log, cache):
+        '''
+        Execute the client request. If the proxy cache contains the response for the request,
+        forward this response back directly from the cache. Otherwise, contact the server
+        to retrieve the information.
+        '''
+
         try:
+            # the md5hash of the modified request is used as the request ID
             md5hash = hashlib.md5()
             md5hash.update(str.encode(self.decoded_client_request.get_modified_request()))
             request_digest = md5hash.hexdigest()
@@ -312,9 +412,10 @@ class ClientRequest:
                 print("%s forwarded from cache.\n" % (request_digest))
 
             else: 
+                # get the information from the server
                 remote_conn = socket.socket(self.socket_family, self.socket_type)
                 remote_conn.connect((host, self.port))
-                remote_conn.settimeout(10.0)
+                remote_conn.settimeout(10.0) # time out after 10 seconds
 
                 if  self.decoded_client_request.method == 'CONNECT':
                     self.local_conn.send(HTTP_VERSION+' 200 Connection established\nProxy-agent: %s\n\n'%PROXY_NAME)
@@ -324,10 +425,12 @@ class ClientRequest:
                     request_size = len(self.decoded_client_request.get_modified_request())                            
                     total_sent = 0
 
+                    # send the request to the server
                     while total_sent < request_size:
                         sent = remote_conn.send(str.encode(self.decoded_client_request.get_modified_request()))
                         total_sent += sent
 
+                # send the response to the client
                 response = b''
                 while True:
                     recvd = remote_conn.recv(BUFFER_LENGTH)
@@ -338,9 +441,7 @@ class ClientRequest:
                         break
 
                 response_size = len(response)
-
                 print("--- Server Response Fowarded ---\n")
-                
                 remote_conn.close()
 
                 # add response to cache
@@ -359,6 +460,10 @@ class ClientRequest:
                 cache.lock.release()
 
 class ProxyConn:
+    '''
+    A proxy connection, between a client and server. This creates the request data structures
+    and then executes the request.
+    '''
                 
     def __init__(self, client_conn, address, strip_cache_headers, strip_user_agent, timeout, log, cache):
         self.client_conn = client_conn
@@ -381,6 +486,9 @@ class InvalidRequest(Exception):
         return repr(self.value)
 
 class Log:
+    '''
+    The proxy log, consisting of the log file and the log lock.
+    '''
 
     def __init__(self):
         self.log_file = open('proxy.log', 'a')
@@ -390,11 +498,17 @@ class Log:
         self.log_file.close()
 
     def append(self, ip, response_size):
+        '''
+        Acquire the log lock and then append the request/response information.
+        '''
         self.log_lock.acquire()
         self.log_file.write("%s %s %i\n" % (str(datetime.datetime.now()), ip, response_size))
         self.log_lock.release()
 
 def start_server(host='localhost', port=4444, IPv6=False, strip_cache_headers=True, strip_user_agent=True, timeout=30):
+    '''
+    Start the proxy server socket and spawn ProxyConnections for each incoming client conneciton.
+    '''
 
     # socket settings
     if IPv6:
@@ -409,10 +523,9 @@ def start_server(host='localhost', port=4444, IPv6=False, strip_cache_headers=Tr
     cache = Cache(CACHE_SIZE)
         
     try:
-
         # initialize socket
         server_socket = socket.socket(socket_family, socket_type)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # remove after testing
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # may be removed in production setting
         server_socket.bind((host, port))
         server_socket.listen(5)
 
